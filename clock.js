260e3f3b3d7ef19bb7956d1d144dfc0d644d60ba
Kirsty Stewart <73973959+klstew142@users.noreply.github.com>
2021-02-08 16:34:05 +0000

    Added clock project files

---
+++ clock.js
@@ -0,0 +1,19 @@
+let clock = () => {
+    let date = new Date();
+    let hrs = date.getHours();
+    let mins = date.getMinutes();
+    let secs = date.getSeconds();
+
+
+//Makes the clock show double figures at all times
+    hrs = hrs < 10 ? "0" + hrs : hrs;
+    mins = mins < 10 ? "0" + mins : mins;
+    secs = secs < 10 ? "0" + secs : secs;
+
+    let time = `${hrs}:${mins}:${secs}`
+    //refresh time - 1 second
+      setInterval(clock, 1000);
+      document.getElementById("clock").innerText = time;
+
+}
+    clock();
